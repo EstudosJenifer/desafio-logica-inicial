54b26f0e68c50aa7192d898c2b8c0608eb496a49
EstudosJenifer <estudosjenifer2023@gmail.com>
2023-11-14 11:43:40 -0300

    meu primeiro commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Olá mundão!!")
+// para mandar o que fiz recente na minha pasta para o github devo salvar o arquivo
+// abrir o terminal e realizar os seguintes comandos:
+// git init -> vai criar uma pasta oculta para mostrar que é versionado no git
+// git add _______(nome do arquivo) - para adicionar os arquivos que criou para dentro do pacote do git _______(nome)
+// caso queira adicionar todos os arquivos dentro da pasta é só colocar git add .
